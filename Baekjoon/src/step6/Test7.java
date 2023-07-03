24f43c9799dff8891ab427df732c430905f477dc
tkddu1591 <tkddu1591@gmail.com>
2023-07-03 17:46:49 +0900

    baekjoon Ch06 Test7번 추가

--- Baekjoon/src/step6/Test7.java
+++ Baekjoon/src/step6/Test7.java
@@ -5,39 +5,32 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Test7 {
 	public static void main(String[] args) throws IOException {
 		BufferedWriter bw =new BufferedWriter(new OutputStreamWriter(System.out));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String st=br.readLine();
-		st = st.toUpperCase();
-		char[] cr = st.toCharArray();
-		int numbers[]  = new int[26];
-		for (int i =0 ;i<cr.length; i++) {
-			numbers[(int)(cr[i])-65] ++;
-		}
-		
-		int order = 0;
-		int max = 0;
-		
-		for(int i=0; i<numbers.length;i++) {
-			if(numbers[i]>max) {
-				max=numbers[i];
-				order=i;
+		int num=Integer.parseInt(br.readLine());
+		String word;
+		int check = 0;
+		Set<Character>  set = new HashSet<>();
+		for(int i=0; i<num; i++){
+			check++;
+			set.clear();
+			word = br.readLine();
+			set.add(word.charAt(0));
+			for(int j =1; j<word.length(); j++){
+				if(set.contains(word.charAt(j))&&word.charAt(j-1)!=word.charAt(j)){
+					check --;
+					break;
+				}
+				set.add(word.charAt(j));
 			}
-			else if(max==numbers[i])
-				order=26;
+
 		}
-		char alpa;
-		if(order!=26) {
-			alpa = (char)(order+65);
-			bw.write(String.valueOf(alpa));
-		}
-		else
-			bw.write("?");
-		
-		
+		bw.write(String.valueOf(check));
 		bw.flush();
 		bw.close();
 	}
